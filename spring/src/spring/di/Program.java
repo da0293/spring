304911faca9a90a9a108ebbfc7c4fdf5a5b2086c
da0293 <dy7133@naver.com>
2024-03-07 23:55:55 +0900

    commit 값DI, 생성자 DI

--- spring/src/spring/di/Program.java
+++ spring/src/spring/di/Program.java
@@ -4,26 +4,15 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import spring.di.ui.ExamConsole;
+import spring.entity.Exam;
 
 public class Program {
 
 	public static void main(String[] args) {
-		/*
-		스프링에게 지시하는 방법으로 코드를 변경
-		1. 부품 생성
-		Exam exam = new NewlecExam();
-		2. 부품 조립
-		ExamConsole console = new InlineExamConsole();
-		ExamConsole console = new GridExamConsole(exam); 
-		3. 결합
-		console.setExam(exam);// DI
-		이 관계를 설정으로 따로 파일로 뺄 것이다.설정으로 뺀 것을 객체화 해서 다시 전달을 해주는 그것이 스프링이다. 
-		*/
 		ApplicationContext context = 
 				new ClassPathXmlApplicationContext("spring/di/setting.xml");
-		// 빈 이름(id)으로 꺼내온다. 형변환해야한다.
-		//ExamConsole console=(ExamConsole)context.getBean("console"); 
-		// 자료형명으로 꺼내온다.형변환할필요없고 클래스명을 깔끔하게 설정하므로 선호함
+		Exam exam =context.getBean(Exam.class);
+		System.out.println(exam.toString());
 		ExamConsole console = context.getBean(ExamConsole.class);
 		console.print();
 	}
