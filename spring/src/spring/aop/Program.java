5d98988cd586bfe12ac1763211a9c86229331e4d
da0293 <dy7133@naver.com>
2024-03-12 23:59:51 +0900

    commit 프록시 적용하여 스프링으로 AOP구현

--- spring/src/spring/aop/Program.java
+++ spring/src/spring/aop/Program.java
@@ -4,19 +4,27 @@ import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import spring.aop.entity.Exam;
 import spring.aop.entity.NewlecExam;
+import spring.di.NewlecDIConfig;
 
 
 public class Program {
 
 	public static void main(String[] args) {
-		Exam exam =  new NewlecExam(1, 1, 1, 1);
+		ApplicationContext context = 
+				//new ClassPathXmlApplicationContext("spring/aop/setting.xml"); 같은 역할
+				new ClassPathXmlApplicationContext("spring/aop/setting.xml");
+		Exam exam = (Exam)context.getBean("proxy"); 
+		// proxy를 이용해 업무 로직 실행 시간 계산하는 부수적 기능까지 껴 놓음 
+		System.out.printf("total is %d\n", exam.total());
+		System.out.printf("avg is %f\n", exam.avg());
 		/*
-		 * loader : NewlecExam 객체를 로드 (실제 업무)
-		 * interfaces 
-		 * h : 곁다리 부분을 꺄줘야한다.
-		 */
+		Exam exam =  new NewlecExam(1, 1, 1, 1);
 		Exam proxyExam = (Exam)Proxy.newProxyInstance(NewlecExam.class.getClassLoader(), 
 				new Class[] {Exam.class}, new InvocationHandler() {
 					
@@ -31,12 +39,14 @@ public class Program {
 					}
 				});
 		// 주 업무 로직만
+		 
 		System.out.printf("total is %d\n", exam.total());
 		System.out.printf("avg is %f\n", exam.avg());
 		System.out.println("-----------------");
 		// proxy를 이용해 업무 로직 실행 시간 계산하는 부수적 기능까지 껴 놓음 
 		System.out.printf("total is %d\n", proxyExam.total());
 		System.out.printf("avg is %f\n", proxyExam.avg());
+		*/
 	}
 
 }
