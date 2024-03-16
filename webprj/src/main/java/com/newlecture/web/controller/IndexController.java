202d9fbed85e5735c29dd4143a3483707cfe5dfd
da0293 <dy7133@naver.com>
2024-03-17 02:19:27 +0900

    commit 스프링 컨트롤러 IndexController 작성하기

--- webprj/src/main/java/com/newlecture/web/controller/IndexController.java
+++ webprj/src/main/java/com/newlecture/web/controller/IndexController.java
@@ -1,5 +1,29 @@
 package com.newlecture.web.controller;
 
-public class IndexController {
-	
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class IndexController implements Controller{
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		// 콘솔에 "index controller" 출력
+		System.out.println("index controller");
+		
+		// ModelAndView 객체 생성
+		ModelAndView mv = new ModelAndView();
+		
+		// "data"라는 키와 "Hello Spring MVC"라는 값을 추가
+		mv.addObject("data", "Hello Spring MVC");
+		
+		// 뷰 이름을 "index.jsp"로 설정
+		mv.setViewName("index.jsp");
+		
+		// ModelAndView 객체 반환
+		return mv;
+	}	
 }
+
